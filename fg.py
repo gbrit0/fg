e18a6e441597d8827e3de38f7cb5f625141256a0
Moises <moisesprotazio@gmail.com>
2025-03-28 22:18:15 -0300

    ideias de estrutura pós conversa com o professor

--- fg.py
+++ fg.py
@@ -1,5 +1,5 @@
 import typer
-
+#import argparse
 
 app = typer.Typer()
 
@@ -9,14 +9,21 @@ def update():
     print("Instalando a versão mais recente")
 
 @app.command()
-def start():
+def start(ver:str = "v1"):
     """Inicia a aplicação"""
-    print("Iniciando a aplicação")
+    print("oi")
 
 @app.command()
 def status():
     """Mostra o status da aplicação"""
     print("Mostrando status")
+    #outromodulo.status()
+
+@app.command()
+def available():
+    """Mostra as versões disponivéis"""
+    print("v1")
+    print("v2")
 
 
 if __name__ == "__main__":
